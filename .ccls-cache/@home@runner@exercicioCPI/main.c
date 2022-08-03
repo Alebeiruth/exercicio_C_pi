756c8b0cc023f5bba06a2c0062d882322eeee5e3
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2022-08-03 15:26:07 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@exercicioCPI/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+  char nome[23];
+
+  puts("Entre com um nome e tecle enter");
+  gets(nome);
+  puts("O nome é: ");
+  puts(nome);
+  printf("\n");
+  return 0;
+}
